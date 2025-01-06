5fdc61a69032f048ed9dd684d7b41de0120c3305
Tril0bit <40827004+Tril0bit@users.noreply.github.com>
2025-01-06 22:14:57 +0400

    Update qubaleo.sh

--- qubaleo.sh
+++ qubaleo.sh
@@ -1,17 +1,11 @@
-server_id = $1
-price = $2 
-gpuName = $3 
-gpu_count = $4 
-cpu = $5
-
-echo 'Acquire::http::Proxy "http://222.130.219.211:1080/";' | tee /etc/apt/apt.conf.d/01proxy
-echo 'Acquire::https::Proxy "http://223.205.25.201:8080/";' | tee -a /etc/apt/apt.conf.d/01proxy
+echo 'Acquire::http::Proxy \"http://222.130.219.211:1080/\";' | tee /etc/apt/apt.conf.d/01proxy
+echo 'Acquire::https::Proxy \"http://223.205.25.201:8080/\";' | tee -a /etc/apt/apt.conf.d/01proxy
 apt update -y
 mkdir -p /q
 cd /q
 wget -P /q https://dl.qubic.li/downloads/qli-Client-2.2.1-Linux-x64.tar.gz
 tar -xvzf qli-Client-2.2.1-Linux-x64.tar.gz
-echo '{{"Settings": {{"baseUrl": "https://mine.qubic.li/", "accessToken": "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImRkMjQ0M2Q4LWI5ZGQtNGNmZi05OGYwLTk0Zjc5YWUwY2U2YSIsIk1pbmluZyI6IiIsIm5iZiI6MTcwOTcyOTQ0MCwiZXhwIjoxNzQxMjY1NDQwLCJpYXQiOjE3MDk3Mjk0NDAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.AA66iu1w8vrdbosKl2DCHpSzD2rQLmVWmic_brXEtw49lVAzAPaRXqBO97Je89dp3BlXgpanlogNJem2uyuiEg", "alias": "r_${gpuName}_x{gpuCount}_${server_id}", "trainer": {{"gpu": true, "gpuVersion": "CUDA12"}}, "idleSettings": {{"gpuOnly":true,"command": "/al/aleominer/aleominer","arguments":"-u stratum+ssl://aleo-asia.f2pool.com:4420 -w trbt3.rx{gpuCount}n${server_id}"}}}}}}' > /q/appsettings.json
+echo '{\"Settings\": {\"baseUrl\": \"https://mine.qubic.li/\", \"accessToken\": \"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImRkMjQ0M2Q4LWI5ZGQtNGNmZi05OGYwLTk0Zjc5YWUwY2U2YSIsIk1pbmluZyI6IiIsIm5iZiI6MTcwOTcyOTQ0MCwiZXhwIjoxNzQxMjY1NDQwLCJpYXQiOjE3MDk3Mjk0NDAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.AA66iu1w8vrdbosKl2DCHpSzD2rQLmVWmic_brXEtw49lVAzAPaRXqBO97Je89dp3BlXgpanlogNJem2uyuiEg\", \"alias\": \"rtest\", \"trainer\": {\"gpu\": true, \"gpuVersion\": \"CUDA12\"}, \"idleSettings\": {\"gpuOnly\":true,\"command\": \"/al/aleominer/aleominer\",\"arguments\":\"-u stratum+ssl://aleo-asia.f2pool.com:4420 -w trbt3.rxtest\"}}}' > /q/appsettings.json
 echo '[program:qli-Client]' >> /etc/supervisor/supervisord.conf
 echo 'command=/q/qli-Client' >> /etc/supervisor/supervisord.conf
 echo 'directory=/q' >> /etc/supervisor/supervisord.conf
@@ -21,7 +15,7 @@ echo 'stdout_logfile=/dev/fd/1' >> /etc/supervisor/supervisord.conf
 echo 'stdout_logfile_maxbytes=0' >> /etc/supervisor/supervisord.conf
 mkdir -p /q2
 cp /q/* /q2
-echo '{{"Settings": {{"amountOfThreads": ${cpu}, "allowHwInfoCollect": true, "baseUrl": "https://mine.qubic.li/", "payoutId": "QCEACBTGCPPHEARVNKEZAVOXURADPKOQUBNWCWCJKCWJOANIBAHHROQGNFRE", "alias": "r_${cpu}_${server_id}", "idleSettings": {{"command": "/z/xmrig-6.21.3/xmrig","arguments":"-o zeph.kryptex.network:7777 -u ZEPHs89ZXrJYSiu4Sw2xLdGFveJ1RWi5tPBVewY1XvoYNFrpXPLQsVEJzUvpKX3R5kcWziMi7wNT2bMdyiKEkZYfGn2qrmTgTJY/r_${cpu}_${server_id} -a rx/0 -k --coin zephyr"}}}}}}' > /q2/appsettings.json
+echo '{\"Settings\": {\"amountOfThreads\": $6, \"allowHwInfoCollect\": true, \"baseUrl\": \"https://mine.qubic.li/\", \"payoutId\": \"QCEACBTGCPPHEARVNKEZAVOXURADPKOQUBNWCWCJKCWJOANIBAHHROQGNFRE\", \"alias\": \"test\", \"idleSettings\": {\"command\": \"/z/xmrig-6.21.3/xmrig\",\"arguments\":\"-o zeph.kryptex.network:7777 -u ZEPHs89ZXrJYSiu4Sw2xLdGFveJ1RWi5tPBVewY1XvoYNFrpXPLQsVEJzUvpKX3R5kcWziMi7wNT2bMdyiKEkZYfGn2qrmTgTJY/r_test -a rx/0 -k --coin zephyr\"}}}' > /q2/appsettings.json
 echo '[program:qli-ClientCPU]' >> /etc/supervisor/supervisord.conf
 echo 'command=/q2/qli-Client' >> /etc/supervisor/supervisord.conf
 echo 'directory=/q2' >> /etc/supervisor/supervisord.conf
