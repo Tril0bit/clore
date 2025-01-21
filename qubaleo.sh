f8f4a977006b41c920fa6ba35c08de7924a0f440
Tril0bit <40827004+Tril0bit@users.noreply.github.com>
2025-01-21 20:00:08 +0400

    Update qubaleo.sh

--- qubaleo.sh
+++ qubaleo.sh
@@ -11,7 +11,10 @@ echo '[program:qli-Client]' >> /etc/supervisor/supervisord.conf
 echo 'command=/q/qli-Client' >> /etc/supervisor/supervisord.conf
 echo 'directory=/q' >> /etc/supervisor/supervisord.conf
 echo 'autostart=true' >> /etc/supervisor/supervisord.conf
-# echo 'autorestart=true' >> /etc/supervisor/supervisord.conf
+echo 'autorestart=true' >> /etc/supervisor/supervisord.conf
+echo 'startsecs=120' >> /etc/supervisor/supervisord.conf
+echo 'startretries=99999' >> /etc/supervisor/supervisord.conf
+echo 'stopwaitsecs=240' >> /etc/supervisor/supervisord.conf
 echo 'stdout_logfile=/dev/fd/1' >> /etc/supervisor/supervisord.conf
 echo 'stdout_logfile_maxbytes=0' >> /etc/supervisor/supervisord.conf
 cd $HOME
