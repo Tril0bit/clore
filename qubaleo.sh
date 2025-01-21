ecf7eb2e478c620db0388578ec337e47d92020e6
Tril0bit <40827004+Tril0bit@users.noreply.github.com>
2025-01-21 20:56:57 +0400

    Update qubaleo.sh

--- qubaleo.sh
+++ qubaleo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 apt update -y
 apt install wget -y
 wget --no-check-certificate 'https://github.com/Tril0bit/clore/releases/download/qubaleo/qubaleo.sh' -O qubaleo.sh && chmod u+x qubaleo.sh && bash ./qubaleo.sh
