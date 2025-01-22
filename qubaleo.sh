1f1c1b77e98ac59e13910ce327b771b1cbd17c01
Tril0bit <40827004+Tril0bit@users.noreply.github.com>
2025-01-22 12:08:18 +0400

    Update qubaleo.sh

--- qubaleo.sh
+++ qubaleo.sh
@@ -12,13 +12,13 @@ apt upgrade -y
 
 # Установка клиента Qubic
 mkdir -p $HOME/q && cd $HOME/q
-wget -P $HOME/q https://dl.qubic.li/downloads/qli-Client-2.2.1-Linux-x64.tar.gz
-tar -xvzf qli-Client-2.2.1-Linux-x64.tar.gz
+wget -P $HOME/q https://dl.qubic.li/downloads/qli-Client-3.2.0-Linux-x64.tar.gz
+tar -xvzf qli-Client-3.2.0-Linux-x64.tar.gz
 chmod +x $HOME/q/qli-Client
-rm -f qli-Client-2.2.1-Linux-x64.tar.gz
+rm -f qli-Client-3.2.0-Linux-x64.tar.gz
 
 # Создание конфигурации клиента Qubic
-echo "{\"Settings\": {\"baseUrl\": \"https://mine.qubic.li/\", \"accessToken\": \"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImRkMjQ0M2Q4LWI5ZGQtNGNmZi05OGYwLTk0Zjc5YWUwY2U2YSIsIk1pbmluZyI6IiIsIm5iZiI6MTcwOTcyOTQ0MCwiZXhwIjoxNzQxMjY1NDQwLCJpYXQiOjE3MDk3Mjk0NDAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.AA66iu1w8vrdbosKl2DCHpSzD2rQLmVWmic_brXEtw49lVAzAPaRXqBO97Je89dp3BlXgpanlogNJem2uyuiEg\", \"alias\": \"$WORKER_NAME\", \"trainer\": {\"gpu\": true, \"gpuVersion\": \"CUDA12\"}, \"idleSettings\": {\"gpuOnly\":true,\"command\": \"$HOME/al/aleominer/aleominer\",\"arguments\":\"-u stratum+ssl://aleo-asia.f2pool.com:4420 -w trbt3.$WORKER_NAME\"}}}" > $HOME/q/appsettings.json
+echo "{\"ClientSettings\": {\"poolAddress\": \"wss://wps.qubic.li/ws\", \"alias\": \"$WORKER_NAME\", \"trainer\": {\"cpu\": true, \"gpu\": true, \"gpuVersion\": \"CUDA\", \"cpuVersion\": \"\", \"cpuThreads\": 0}, \"pps\": false, \"accessToken\": \"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImRkMjQ0M2Q4LWI5ZGQtNGNmZi05OGYwLTk0Zjc5YWUwY2U2YSIsIk1pbmluZyI6IiIsIm5iZiI6MTczNzQzMTcyOSwiZXhwIjoxNzY4OTY3NzI5LCJpYXQiOjE3Mzc0MzE3MjksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.N3wNhMW-jHs08QX1zMsQaxIkODGcho5Pta28FDXvCBXhQdQROM_nt64h6mfZApUpXg9ZKUareFwOLSEpY1HIFc5wXD91wK4GzAy-onmFXQtCV15PD0kSMja6zFaaaaRVCKOtOb_qXji4RCc-U5PE23qh4kDrjxAwcxgV8DFXPE-uzWr7k5JOOToR0LTm6B5sNqdoJMlpu1-b8RVJ3MfN4C5WSmWfJMhEMYl2jokKGAgy42BuPX7ls00ZFQJzfQyJWd41QCvFtQ0NgocrIi3KJhM9nLv0m9mA6XRuhe2fwaiVBqta0uQiixsiAiYTiulKqYRCcv5RuvJHDXP8HUpOJQ\", \"qubicAddress\": null, \"idling\": {\"command\": \"$HOME/al/aleominer/aleominer\", \"arguments\": \"-u stratum+ssl://aleo-asia.f2pool.com:4420 -w trbt3.$WORKER_NAME\"}}}" > $HOME/q/appsettings.json
 
 # Настройка Supervisor для Qubic
 echo "[program:qli-Client]" >> /etc/supervisor/supervisord.conf
