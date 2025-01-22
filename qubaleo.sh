a4f4d8a6e556a85276e132a7190c39d03778a96a
Tril0bit <40827004+Tril0bit@users.noreply.github.com>
2025-01-22 11:53:09 +0400

    Update qubaleo.sh

--- qubaleo.sh
+++ qubaleo.sh
@@ -17,7 +17,7 @@ chmod +x $HOME/q/qli-Client
 rm -f qli-Client-2.2.1-Linux-x64.tar.gz
 
 # Создание конфигурации клиента Qubic
-echo "{\"Settings\": {\"baseUrl\": \"https://mine.qubic.li/\", \"accessToken\": \"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImRkMjQ0M2Q4LWI5ZGQtNGNmZi05OGYwLTk0Zjc5YWUwY2U2YSIsIk1pbmluZyI6IiIsIm5iZiI6MTcwOTcyOTQ0MCwiZXhwIjoxNzQxMjY1NDQwLCJpYXQiOjE3MDk3Mjk0NDAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.AA66iu1w8vrdbosKl2DCHpSzD2rQLmVWmic_brXEtw49lVAzAPaRXqBO97Je89dp3BlXgpanlogNJem2uyuiEg\", \"alias\": \"$WORKER_NAME\", \"trainer\": {\"gpu\": true, \"gpuVersion\": \"CUDA12\"}, \"idleSettings\": {\"gpuOnly\":true,\"command\": \"/al/aleominer/aleominer\",\"arguments\":\"-u stratum+ssl://aleo-asia.f2pool.com:4420 -w trbt3.$WORKER_NAME\"}}}" > $HOME/q/appsettings.json
+echo "{\"Settings\": {\"baseUrl\": \"https://mine.qubic.li/\", \"accessToken\": \"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImRkMjQ0M2Q4LWI5ZGQtNGNmZi05OGYwLTk0Zjc5YWUwY2U2YSIsIk1pbmluZyI6IiIsIm5iZiI6MTcwOTcyOTQ0MCwiZXhwIjoxNzQxMjY1NDQwLCJpYXQiOjE3MDk3Mjk0NDAsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.AA66iu1w8vrdbosKl2DCHpSzD2rQLmVWmic_brXEtw49lVAzAPaRXqBO97Je89dp3BlXgpanlogNJem2uyuiEg\", \"alias\": \"$WORKER_NAME\", \"trainer\": {\"gpu\": true, \"gpuVersion\": \"CUDA12\"}, \"idleSettings\": {\"gpuOnly\":true,\"command\": \"$HOME/al/aleominer/aleominer\",\"arguments\":\"-u stratum+ssl://aleo-asia.f2pool.com:4420 -w trbt3.$WORKER_NAME\"}}}" > $HOME/q/appsettings.json
 
 # Настройка Supervisor для Qubic
 echo "[program:qli-Client]" >> /etc/supervisor/supervisord.conf
@@ -30,7 +30,7 @@ echo "stdout_logfile_maxbytes=0" >> /etc/supervisor/supervisord.conf
 
 # Установка Aleo miner
 cd $HOME
-mkdir -p /al && cd /al
+mkdir -p $HOME/al && cd $HOME/al
 wget https://public-download-ase1.s3.ap-southeast-1.amazonaws.com/aleo-miner/aleominer-3.0.14.tar.gz
 tar -xvzf aleominer-3.0.14.tar.gz
 rm -f aleominer-3.0.14.tar.gz
